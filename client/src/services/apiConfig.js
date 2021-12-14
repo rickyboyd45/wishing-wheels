672304d3fb83b29d327ccefb6343facd8351c936
Ricky Boyd <rickyboyd23@gmail.com>
2021-12-14 14:13:49 -0500

    prepping for netlify deployment

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:3000';
+const baseURL =
+  process.env.NODE_ENV === 'production' ?
+    'https://wishing-wheels.herokuapp.com/' : 'http://localhost:3000'
 
 const api = axios.create({
   baseURL: baseURL,
